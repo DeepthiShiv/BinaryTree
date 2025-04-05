04a4075b5d40e5246e511d96fab14e3e0e44fc67
Deepthi S <130971935+DeepthiShiv@users.noreply.github.com>
2025-04-05 09:54:07 +0530

    Update Diameter.java
    Optimized approach

--- Diameter.java
+++ Diameter.java
@@ -27,7 +27,28 @@ public class Main{
          int selfDia= leftH+ rightH+1;
          return Math.max(selfDia, Math.max(leftDia,rightDia));
      }
-     public static void main(String args[]){
+
+    //O(n)
+    static class Info{
+         int dia;
+         int ht;
+         public Info(int dia,int ht){
+             this.dia= dia;
+             this.ht= ht;
+         }
+     }
+      public static Info diameter2(Node root){
+         if(root==null) return new Info(0,0);
+         Info leftInfo= diameter2(root.left);
+         Info rightInfo= diameter2(root.right);
+         
+         int dia= Math.max(Math.max(leftInfo.dia, rightInfo.dia), leftInfo.ht+ rightInfo.ht+1);
+         int ht= Math.max(leftInfo.ht, rightInfo.ht)+1;
+         
+         return new Info(dia, ht);
+     }
+    
+    public static void main(String args[]){
         Node root= new Node(1);
         root.left= new Node(2);
         root.left.left= new Node(4);
@@ -35,8 +56,8 @@ public class Main{
         root.left.right.right= new Node(6);
         root.left.right.right.right= new Node(7);
         root.right= new Node(3);
-        System.out.print(diameter(root));
-    
+        System.out.println(diameter(root));
+        System.out.println(diameter2(root));
      }
     
     
